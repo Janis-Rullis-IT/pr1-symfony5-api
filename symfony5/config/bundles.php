b102a6a6e97929a36c49f156c6d99ecf3af6fd31
Janis Rullis <janis.rullis.ite@gmail.com>
2020-04-10 17:58:26 +0300

    #53 Add psysh interactive tinker

--- symfony5/config/bundles.php
+++ symfony5/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['test' => true],
+    Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
 ];
