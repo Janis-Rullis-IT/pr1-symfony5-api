5134f62a880d249ab213c37afaeeaebf7e469c08
Janis Rullis <janis.rullis.ite@gmail.com>
2020-04-10 21:09:45 +0300

    #53 OrdeShippingTest: Adapt to use fixtures

--- symfony5/tests/Order/OrderShippingTest.php
+++ symfony5/tests/Order/OrderShippingTest.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 use App\Interfaces\IUserRepo;
+use App\User\UserWihProductsGenerator;
 
 /**
  * #40 PUT /users/{customerId}/order/shipping .
@@ -16,14 +17,14 @@ class OrderShippingTest extends WebTestCase
 	private $impossibleInt = 3147483648;
 	private $entityManager;
 	private $ship_to_address = [
-		"name" => "John",
-		"surname" => "Doe",
-		"street" => "Palm street 25-7",
-		"state" => "California",
-		"zip" => "60744",
-		"country" => "US",
-		"phone" => "+1 123 123 123",
-		"is_express" => true
+			"name" => "John",
+			"surname" => "Doe",
+			"street" => "Palm street 25-7",
+			"state" => "California",
+			"zip" => "60744",
+			"country" => "US",
+			"phone" => "+1 123 123 123",
+			"is_express" => true
 	];
 
 	/**
@@ -80,10 +81,13 @@ class OrderShippingTest extends WebTestCase
 	/**
 	 * #40 Valid request.
 	 */
-	public function todoTestValidRequest()
+	public function testValidRequest()
 	{
 		$client = static::createClient();
-		$user = $this->insertUsersAndProds($client)[0];
+		$this->c = $client->getContainer();
+		$this->entityManager = $this->c->get('doctrine')->getManager();
+		$this->userWithProductsGenerator = $this->c->get('test.' . UserWihProductsGenerator::class);
+		$user = $this->userWithProductsGenerator->generate(1)[0];
 
 		$customerId = $user->getId();
 		$productId = $user->getProducts()[0]->getId();
@@ -105,72 +109,5 @@ class OrderShippingTest extends WebTestCase
 		foreach ($data as $key => $val) {
 			$this->assertEquals($val, $responseBody[$key]);
 		}
-	}
-
-	/**
-	 * #38 Create 3 users with 1 mug and 1 shirt.
-	 * 
-	 * TODO: Replace this approach with fixtures or creators that are designed not just for access from controllers.
-	 * 
-	 * @return User array
-	 */
-	private function insertUsersAndProds($client, int $count = 1)
-	{
-		$this->c = $client->getContainer();
-		$this->entityManager = $this->c->get('doctrine')->getManager();
-		$this->userRepo = $this->c->get('test.' . IUserRepo::class);
-		return $this->userRepo->createQueryBuilder('a')->orderBy('a.id', 'DESC')->setMaxResults(3)->getQuery()->getResult();
-
-		// #38 Create 3 users.
-		$users = [];
-		for ($i = 0; $i < $count; $i++) {
-
-			$users[$i] = $user = $this->createUser($i);
-
-			// #38 Create 1 mug and 1 shirt for each user.
-			$user->products = [];
-			$productTypes = ['t-shirt', 'mug'];
-			foreach ($productTypes as $productType) {
-				$user->products[] = $this->createUserProduct($user, $productType);
-			}
-		}
-		return $users;
-	}
-
-	/**
-	 * #40 Create a user.
-	 * 
-	 * @param type $i
-	 * @return User
-	 */
-	private function createUser($i): User
-	{
-		$user = new User();
-		$user->setName(rand());
-		$user->setSurname($i + 1);
-		$user->setBalance(1000);
-		$this->entityManager->persist($user);
-		$this->entityManager->flush();
-		return $user;
-	}
-
-	/**
-	 * #40 Create a product.
-	 * 
-	 * @param User $user
-	 * @param string $productType
-	 * @return Product
-	 */
-	private function createUserProduct(User $user, string $productType): Product
-	{
-		$product = new Product();
-		$product->setOwnerId($user->getId());
-		$product->setType($productType);
-		$product->setTitle($user->getName() . ' ' . $user->getSurname() . ' ' . $productType);
-		$product->setSku($user->getName() . ' ' . $user->getSurname() . ' ' . $productType);
-		$product->setCost(100);
-		$this->entityManager->persist($product);
-		$this->entityManager->flush();
-		return $product;
 	}
 }
