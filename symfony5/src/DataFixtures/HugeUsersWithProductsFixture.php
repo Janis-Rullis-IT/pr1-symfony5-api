84034b6e07814df74d015d5a49208a5e09cfc44d
Janis Rullis <janis.rullis.ite@gmail.com>
2020-04-11 21:13:56 +0300

    #57 HugeUsersWithProductsFixture: Exec generate_data() only if there's a small amount of data

--- symfony5/src/DataFixtures/HugeUsersWithProductsFixture.php
+++ symfony5/src/DataFixtures/HugeUsersWithProductsFixture.php
@@ -8,32 +8,35 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use \App\User\UserWihProductsGenerator;
+use \App\Interfaces\IUserRepo;
 
 class HugeUsersWithProductsFixture extends Fixture implements FixtureGroupInterface
 {
 
 	private $userWithProductsGenerator;
+	private $userRepo;
 
 	public static function getGroups(): array
 	{
 		return ['huge', 'users', 'users_with_products'];
 	}
 
-	public function __construct(UserWihProductsGenerator $userWithProductsGenerator)
+	public function __construct(UserWihProductsGenerator $userWithProductsGenerator, IUserRepo $userRepo)
 	{
 		$this->userWithProductsGenerator = $userWithProductsGenerator;
+		$this->userRepo = $userRepo;
 	}
 
 	public function load(ObjectManager $manager)
 	{
-		
-		// #57 Fill tables using a stored procedure.
-		//TRUNCATE `user`;
-		//TRUNCATE `product`;
-		//TRUNCATE `order`;
-		//TRUNCATE `order_product`;
-		//
-		//CALL generate_data();
-//		$this->userWithProductsGenerator->generate(100000);
+		// #57 Don't execute the data generation if there's already a huge dataset.
+		if ($this->userRepo->count([]) > 1000000) {
+			return;
+		} else {
+			// #57 This will take ~3min.
+			$conn = $manager->getConnection();
+			$stmt = $conn->prepare('CALL generate_data()');
+			return $stmt->execute();
+		}
 	}
 }
